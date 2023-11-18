9e3edb912935eb3dad17e899809de2c272814e63
acidicMercury8 <acidicmercury8@outlook.com>
2023-11-18 22:41:59 +0300

    Add build recipe for LuaJIT dated 2023.08.23
    Co-authored-by: ForserX <sv3nk@yandex.ru>

---
+++ pkg/IXRay.Packages.LuaJIT/2023.8.23-open/build-x86.bat
@@ -0,0 +1,3 @@
+call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+cd dep\LuaJIT\src
+call msvcbuild.bat
---
+++ pkg/IXRay.Packages.LuaJIT/2023.8.23-open/invoke.ps1
@@ -0,0 +1,32 @@
+$Destination = "dep/LuaJIT"
+
+$Output = "out"
+
+function Invoke-Patch {
+    Copy-Item -Path $Destination/README -Destination $Destination/README.md
+}
+
+function Invoke-Build {
+    New-Item -Name $Destination/out/x86 -ItemType directory
+    New-Item -Name $Destination/out/x64 -ItemType directory
+    
+    & $PSScriptRoot/build-x86.bat
+    Move-Item -Path "$Destination/src/*.dll" -Destination "$Destination/out/x86/"
+    Move-Item -Path "$Destination/src/*.lib" -Destination "$Destination/out/x86/"
+    Move-Item -Path "$Destination/src/*.exp" -Destination "$Destination/out/x86/"
+
+    & $PSScriptRoot/build-x64.bat
+    Move-Item -Path "$Destination/src/*.dll" -Destination "$Destination/out/x64/"
+    Move-Item -Path "$Destination/src/*.lib" -Destination "$Destination/out/x64/"
+    Move-Item -Path "$Destination/src/*.exp" -Destination "$Destination/out/x64/"
+}
+
+function Invoke-Pack {
+    nuget pack $PSScriptRoot\IXRay.Packages.LuaJIT.nuspec -OutputDirectory $Output
+}
+
+function Invoke-Actions {
+    Invoke-Patch
+    Invoke-Build
+    Invoke-Pack
+}
